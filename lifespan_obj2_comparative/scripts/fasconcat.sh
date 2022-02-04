78c65a4f098cc24d60d732ea13974d635f821848
Tanya Lama <36007010+tanyalama@users.noreply.github.com>
2022-02-04 08:54:23 -0500

    Update fasconcat.sh

--- lifespan_obj2_comparative/scripts/fasconcat.sh
+++ lifespan_obj2_comparative/scripts/fasconcat.sh
@@ -5,7 +5,7 @@
 #SBATCH -o slurm-%j.out  # %j = job ID
 #SBATCH --ntasks-per-node=40
 #SBATCH --nodes=1
-#SBATCH --time=96:00:00
+#SBATCH --time=1:00:00
 #SBATCH -p gpu-long
 
-perl ./FASconCAT-G_v1.05.pl -s ##this will produce a supermatrix and all info; you can add -n or -p if you need other inputs
+perl ./FASconCAT-G_v1.05.pl -s #this only ran for 1718.25 seconds or 30 minutes. This needs to be run from within your /fasta and FASconCAT folder. 
